cf4c955f142a7c5ff2ded9eff469b47fa85f6e3a
Sinhika <sinhika@republicofnewhome.org>
2016-10-09 09:36:11 -0500

    added update_libs.sh to utils

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Small utility scripts to help me with mod development.
 
 - **build_graphic_jsons.pl**: use an vanilla block or item as a template to create mod blockstate and model jsons.
+- **update_libs.sh**: fix up links to latest versions of required mods for compiling.
---
+++ update_libs.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/bash
+set +f
+
+TOPDIR=${HOME}/Projects/Minecraft_1.10
+DEVLIB=${TOPDIR}/devlibs
+DEOBFLIST="SimpleCore SimpleOres2 Fusion Netherrocks"
+NEEDSLIBLIST="SimpleOres2 Fusion Netherrocks Aesthetics"
+
+if [ ! -d ${DEVLIB} ]; then
+    mkdir -p ${DEVLIB}
+fi
+
+echo "Cleaning existing libs..."
+cd ${DEVLIB}
+rm -f *.jar
+
+echo "Create libs directories if needed..."
+for D in ${NEEDSLIBLIST}; do
+    if [ ! -d ${TOPDIR}/${D}/libs ]; then
+        mkdir -p ${TOPDIR}/${D}/libs
+    fi
+done
+
+for D in ${DEOBFLIST}; do
+    if [ ! -d ${TOPDIR}/${D}/build/libs ]; then
+        continue
+    fi
+    JARLIST=`ls -v -r ${TOPDIR}/${D}/build/libs/*-deobf.jar`
+    JAR=`echo ${JARLIST}  | cut -d' ' -f1`
+    if [ -r ${JAR} ]; then
+        cp -v ${JAR} .
+    fi
+done
+cd ${TOPDIR}/SimpleOres2/libs
+rm -f *.jar
+ln -v -s ${DEVLIB}/simplecore-*.jar
+exit
+cd ${TOPDIR}/Fusion/libs
+rm -f *.jar
+ln -v -s ${DEVLIB}/simplecore-*.jar .
+ln -v -s ${DEVLIB}/simpleores-*.jar .
+
+cd ${TOPDIR}/Netherrocks/libs
+rm -r *.jar
+ln -v -s ${DEVLIB}/simplecore-*.jar .
+
+cd ${TOPDIR}/Aesthetics/libs
+rm -f *.jar
+ln -v -s ${DEVLIB}/simplecore-*.jar .
+ln -v -s ${DEVLIB}/simpleores-*.jar .
+ln -v -s ${DEVLIB}/fusion-*.jar .
+ln -v -s ${DEVLIB}/netherrocks-*.jar .
