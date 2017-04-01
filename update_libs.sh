da12ec805d65fe0fa2d44cc5d88dba06481992c3
Sinhika <sinhika@republicofnewhome.org>
2017-04-01 12:07:15 -0500

    version with 1.11.2 libs; removed need for Netherrocks directory in 1.11.2

--- update_libs.sh
+++ update_libs.sh
@@ -1,57 +1,56 @@
-#!/usr/bin/bash
-set +f
+#!/bin/bash
 
-TOPDIR=${HOME}/Projects/Minecraft_1.11
+TOPDIR=${HOME}/Projects/Minecraft_1.10
 DEVLIB=${TOPDIR}/devlibs
 DEOBFLIST="SimpleCore SimpleOres2 Fusion Netherrocks"
-NEEDSLIBLIST="SimpleOres2 Fusion Netherrocks Aesthetics"
-IS_CYGWIN=1
-if [ $IS_CYGWIN == 1 ]; then
-    COPYCMD="cp -v"
-else
-    COPYCMD="ln -v -s"
-fi
-if [ ! -d ${DEVLIB} ]; then
-    mkdir -p ${DEVLIB}
-fi
 
-echo "Cleaning existing libs..."
+echo "Cleaning existing 1.10 libs..."
 cd ${DEVLIB}
 rm -f *.jar
 
-echo "Create libs directories if needed..."
-for D in ${NEEDSLIBLIST}; do
-    if [ ! -d ${TOPDIR}/${D}/libs ]; then
-        mkdir -p ${TOPDIR}/${D}/libs
-    fi
-done
-
 for D in ${DEOBFLIST}; do
-    if [ ! -d ${TOPDIR}/${D}/build/libs ]; then
-        continue
-    fi
     JARLIST=`ls -v -r ${TOPDIR}/${D}/build/libs/*-deobf.jar`
     JAR=`echo ${JARLIST}  | cut -d' ' -f1`
-    if [ -r ${JAR} ]; then
-        cp -v ${JAR} .
-    fi
+    cp -v ${JAR} .
 done
+
 cd ${TOPDIR}/SimpleOres2/libs
 rm -f *.jar
-${COPYCMD} ${DEVLIB}/simplecore-*.jar .
+ln -v -s ${DEVLIB}/simplecore-*.jar .
 
 cd ${TOPDIR}/Fusion/libs
 rm -f *.jar
-${COPYCMD} ${DEVLIB}/simplecore-*.jar .
-${COPYCMD} ${DEVLIB}/simpleores-*.jar .
+ln -v -s ${DEVLIB}/simplecore-*.jar .
+ln -v -s ${DEVLIB}/simpleores-*.jar .
 
 cd ${TOPDIR}/Netherrocks/libs
 rm -r *.jar
-${COPYCMD} ${DEVLIB}/simplecore-*.jar .
+ln -v -s ${DEVLIB}/simplecore-*.jar .
 
 cd ${TOPDIR}/Aesthetics/libs
 rm -f *.jar
-${COPYCMD} ${DEVLIB}/simplecore-*.jar .
-${COPYCMD} ${DEVLIB}/simpleores-*.jar .
-${COPYCMD} ${DEVLIB}/fusion-*.jar .
-${COPYCMD} ${DEVLIB}/netherrocks-*.jar .
+ln -v -s ${DEVLIB}/simplecore-*.jar .
+ln -v -s ${DEVLIB}/simpleores-*.jar .
+ln -v -s ${DEVLIB}/fusion-*.jar .
+ln -v -s ${DEVLIB}/netherrocks-*.jar .
+
+cd ${TOPDIR}/Machines/libs
+rm -f *.jar
+ln -v -s ${DEVLIB}/simplecore-*.jar .
+ln -v -s ${DEVLIB}/simpleores-*.jar .
+
+
+TOPDIR=${HOME}/Projects/Minecraft_1.11
+DEVLIB=${TOPDIR}/devlibs
+DEOBFLIST="SimpleCore"
+
+echo "Cleaning existing 1.11 libs..."
+cd ${DEVLIB}
+rm -f *.jar
+
+for D in ${DEOBFLIST}; do
+    JARLIST=`ls -v -r ${TOPDIR}/${D}/build/libs/*-deobf.jar`
+    JAR=`echo ${JARLIST}  | cut -d' ' -f1`
+    cp -v ${JAR} .
+done
+
